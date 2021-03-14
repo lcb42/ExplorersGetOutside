6e37bfa1c67f46f58ca4ab357c3a648c4c7a5b70
lcb42 <blatherwick.lucy@gmail.com>
2021-03-14 00:20:16 +0000

    Fixed layout and added paper tiles

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Grid } from '@material-ui/core';
+import { Grid, Container, Paper } from '@material-ui/core';
 
 import { MapContainer, TileLayer } from 'react-leaflet'
 
@@ -16,11 +16,16 @@ export default function Home() {
             url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
             />
         </MapContainer>
-        <Grid container className="page-container">
-            <Grid item xs={12} md={3} className="data-container">
-                <p>Data</p>
-            </Grid>
-        </Grid> 
+        <div className="sidebar">
+                <Container className="title-container">
+                    <h1>GetOutside!</h1>
+                    <h4>sotoncityexplorers</h4>
+                </Container>
+                <Paper className="paper-tile" elevation={3} square>Boop</Paper>
+                <Paper className="paper-tile" elevation={3} square>Stuff</Paper>
+                <Paper className="paper-tile" elevation={3} square>Other stuff</Paper>
+                <Paper className="paper-tile" elevation={3} square>More stuff</Paper>
+        </div>
         </>  
     )
 }
