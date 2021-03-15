f7fa42b902b610a0203c27ccd739b4b3a0c3a9fa
lcb42 <blatherwick.lucy@gmail.com>
2021-03-15 21:34:17 +0000

    Created info page outline

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,15 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 
 import Home from './Home.js';
+import InfoPage from './InfoPage.js';
 
 export default function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/info">
+          <InfoPage />
+        </Route>
         <Route path="/">
           <Home />
         </Route>
--- client/src/InfoPage.js
+++ client/src/InfoPage.js
@@ -5,9 +5,9 @@ import { Container, Paper } from '@material-ui/core';
 import { MapContainer, TileLayer } from 'react-leaflet'
 
 import './App.css';
-import './Home.css';
+import './InfoPage.css';
 
-export default function Home() {
+export default function InfoPage() {
     const position = [55, 1]
     return (
         <>
@@ -17,15 +17,15 @@ export default function Home() {
             url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
             />
         </MapContainer>
-        <div className="home-contents contents">
-                <Container className="home-title-container">
+        <div className="infopage-contents contents">
+                <Container className="infopage-title-container">
                     <h1>GetOutside!</h1>
                     <h4>sotoncityexplorers</h4>
                 </Container>
-                <Paper className="paper-tile" elevation={3} square>Boop</Paper>
-                <Paper className="paper-tile" elevation={3} square>Stuff</Paper>
-                <Paper className="paper-tile" elevation={3} square>Other stuff</Paper>
-                <Paper className="paper-tile" elevation={3} square>More stuff</Paper>
+                <Paper className="paper-tile infopage-paper-tile" elevation={3} square>Boop</Paper>
+                <Paper className="paper-tile infopage-paper-tile" elevation={3} square>Stuff</Paper>
+                <Paper className="paper-tile infopage-paper-tile" elevation={3} square>Other stuff</Paper>
+                <Paper className="paper-tile infopage-paper-tile" elevation={3} square>More stuff</Paper>
         </div>
         </>  
     )
