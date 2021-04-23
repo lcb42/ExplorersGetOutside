93997653a6e61178c6a46758746e6cf26deffa1f
lcb42 <blatherwick.lucy@gmail.com>
2021-04-23 23:48:30 +0100

    Added mysql database

--- server.js
+++ server.js
@@ -2,10 +2,32 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+var mysql = require('mysql')
+const db = require('./db.js');
+var connection = mysql.createConnection(db)
+
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+connection.connect()
 
 // create a GET route
 app.get('/express_backend', (req, res) => {
     res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+
+    connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+        if (err) throw err
+      
+        console.log('The solution is: ', rows[0].solution)
+    })
+
+      
+    connection.end()
 });
+
+
+
+
+
+
+
